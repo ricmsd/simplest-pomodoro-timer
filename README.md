95da9eff50811cdaafc229c4f9d5bca2d1098d70
RYUICHI MASUDA <ricmsd@gmail.com>
2023-01-28 14:38:11 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simplest Pomodoro Timer
+Pomodoro Timer (https://ricmsd.github.io/timer/pomodoro1/) as a desktop application.
+
+## requirements
+- Node.js
+- yarn
+
+## install & execute
+    npm install
+    npm run start
+
+## generate .exe
+    yarn run pack
+
+## TODO
+- Hide title bar
+- Save the window display location
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+        alwaysOnTop: true,
+        opacity: 0.75
+    })
+
+    win.setMenu(null);
+    //win.loadFile('index.html')
+    win.loadURL('https://ricmsd.github.io/timer/pomodoro1/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
